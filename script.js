86346f852685f433de393ea0e69c9abea1c27c76
dandan-187 <danterg187@gmail.com>
2025-03-16 00:29:58 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// script.js
-document.getElementById("openMessenger").addEventListener("click", function() {
-    window.open("https://www.messenger.com/t/100011166947174", "_blank", "noopener,noreferrer");
-});
