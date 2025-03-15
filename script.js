289cfd1ed424f05a7c1b0dc3e6ef67525f81bea1
dandan-187 <danterg187@gmail.com>
2025-03-16 02:25:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 document.getElementById("openMessenger").addEventListener("click", function() {
-    const userId = "100011166947174";
-    const appLink = "fb://messaging/" + userId; // Try "fb://profile/" if this fails
-    const fallbackWebLink = "https://www.messenger.com/t/" + userId;
-
-    // Try opening Messenger app
-    window.location.href = appLink;
-
-    // Fallback: Open Messenger in browser after 2s if app doesn't open
-    setTimeout(() => {
-        window.location.href = fallbackWebLink;
-    }, 2000);
+    window.location.href = "fb-messenger://user-thread/100011166947174";
 });
