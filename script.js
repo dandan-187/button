75a8487b2c91a28026edf19a073639aa20d3a88c
dandan-187 <danterg187@gmail.com>
2025-03-16 00:16:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// script.js
 document.getElementById("openMessenger").addEventListener("click", function() {
-    window.location.href = "fb://messaging/100011166947174";
+    window.open("https://www.messenger.com/t/100011166947174", "_blank");
 });
