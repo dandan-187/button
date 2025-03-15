994c76f100f06554dd7471dd5b71b5cf9d8a3db8
dandan-187 <danterg187@gmail.com>
2025-03-16 00:36:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.getElementById("openMessenger").addEventListener("click", function() {
     const userId = "100011166947174";
     
-    // Check if the user is on mobile or desktop
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        // Open Messenger App on Mobile
-        window.location.href = "fb-messenger://user-thread/" + userId;
-    } else {
-        // Open Messenger in a new tab on PC
-        window.open("https://www.messenger.com/t/" + userId, "_blank");
-    }
+    // Open Messenger App on Mobile
+    window.location.href = "fb-messenger://user-thread/" + userId;
+
+    // Fallback: Open Messenger in browser if the app is not installed
+    setTimeout(() => {
+        window.location.href = "https://www.messenger.com/t/" + userId;
+    }, 1500);
 });
